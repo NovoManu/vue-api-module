d006c5786461dc69faff9904480cbfa530371578
Manu <operamike@mail.ru>
2020-10-25 18:52:05 +0100

    Initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,5 @@ module.exports = {
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
 };
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Vue API module example</h1>
+    <p>Please open console</p>
+    <p>And check VUEX store</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
 };
 </script>
 
